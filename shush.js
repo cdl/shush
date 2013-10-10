f144fa6431c11232594394ec4c81cb8b9c50236a
Colby Ludwig <me@colbyludwig.com>
2013-10-09 18:43:51 -0600

    Got rid of now unnecessary braces around if blocks

--- shush.js
+++ shush.js
@@ -7,18 +7,16 @@
 		if (typeof($btn) == 'string') {
 			$btn = $form.find($btn)[0];
 			if ($btn.length == 0) {
-				if (window.console) {
+				if (window.console)
 					console.error("Couldn't find element with selector " + btnSelector);
-				}
 				
 				return false;
 			}
 		} else if ($btn instanceof Object) {
 			$btn = $form.children(btnSelector[0])[0];
 		} else {
-			if (window.console) {
+			if (window.console)
 				console.error("Expected parameter type 'string', 'jQuery', or 'Object', got " + typeof(btnSelector));
-			}
 			
 			return false;
 		}
